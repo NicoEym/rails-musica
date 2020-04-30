81194b5a4e46fe42456dbba668140d807173ed3b
nicolas <nicolas.eymenier@gmail.com>
2020-04-29 22:37:06 -0300

    solve

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,15 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.where(level: params[:level], style: params[:style])
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:level, :style)
+  end
+end
